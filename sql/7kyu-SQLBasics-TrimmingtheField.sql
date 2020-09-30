4b1dc2e41d596539f755b6694f0c22463903332c
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2020-09-30 16:22:33 -0300

    Delete 7kyu-SQLBasics-TrimmingtheField.sql

--- sql/7kyu-SQLBasics-TrimmingtheField.sql
+++
@@ -1,8 +0,0 @@
-SELECT id, 
-       name, 
-       CASE
-         WHEN STRPOS(characteristics, ',') > 0 THEN SUBSTRING(characteristics, 0, STRPOS(characteristics, ','))
-         WHEN STRPOS(characteristics, ',') = 0 THEN characteristics
-       END AS characteristic 
-FROM monsters
-ORDER BY id
