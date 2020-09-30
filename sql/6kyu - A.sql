a1f5ffca62d86fbd971da7e7e0cd352ef3b6386e
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2020-09-30 16:24:59 -0300

    Create 6kyu - A.sql

---
+++ sql/6kyu - A.sql
@@ -0,0 +1,7 @@
+--Link -> https://www.codewars.com/kata/58112f8004adbbdb500004fe
+
+SELECT 'US' AS location, * FROM ussales
+WHERE price > 50.00
+UNION ALL
+SELECT 'EU' AS location, * FROM eusales
+WHERE price > 50.00
