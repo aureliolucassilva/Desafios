89c6c884701644d3ff8fd9e5995bc3698ecf07d4
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2020-09-30 16:21:56 -0300

    Delete 6kyu-SQLBasicsTop10customersbytotalpaymentsamount.sql

--- sql/6kyu-SQLBasicsTop10customersbytotalpaymentsamount.sql
+++
@@ -1,9 +0,0 @@
-SELECT payment.customer_id,
-       email,
-       COUNT(payment_id) AS payments_count,
-       SUM(amount)::FLOAT AS total_amount
-FROM payment
-INNER JOIN customer ON payment.customer_id = customer.customer_id
-GROUP BY payment.customer_id, email
-ORDER BY total_amount DESC
-LIMIT 10
