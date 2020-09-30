2e708719ab3b62f0bd1ecd3d21d650f4eb1d0742
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2020-09-30 17:31:34 -0300

    Create 7kyu - C.js

---
+++ javascript/7kyu - C.js
@@ -0,0 +1,53 @@
+//Link -> https://www.codewars.com/kata/558fa34727c2d274c10000ae
+
+function scrabbleScore(str){
+    const newString = Array.from(str.toUpperCase());
+    var score = 0;
+
+    for(const letter of newString){
+        switch(letter){
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'L':
+            case 'N':
+            case 'R':
+            case 'S':
+            case 'T':
+                score += 1;
+                break;
+            case 'D':
+            case 'G':
+                score += 2;
+                break;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                score += 3;
+                break;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                score += 4;
+                break;
+            case 'K':
+                score += 5;
+                break;
+            case 'J':
+            case 'X':
+                score += 8;
+                break;
+            case 'Q':
+            case 'Z':
+                score += 10;
+                break;
+        }
+    }
+
+    return score;
+}
