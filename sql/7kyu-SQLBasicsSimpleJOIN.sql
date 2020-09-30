b9efa9cf49a0eeeb176365dbf0e5a4fa715eb6ad
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2020-09-30 16:23:02 -0300

    Delete 7kyu-SQLBasicsSimpleJOIN.sql

--- sql/7kyu-SQLBasicsSimpleJOIN.sql
+++
@@ -1,8 +0,0 @@
-SELECT products.id AS id, 
-       products.name AS name, 
-       isbn, 
-       company_id,
-       price,
-       companies.name AS company_name
-FROM products
-INNER JOIN companies ON products.company_id = companies.id
