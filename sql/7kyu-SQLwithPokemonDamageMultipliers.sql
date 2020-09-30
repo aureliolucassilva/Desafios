53d47343a233a5ef7ba6792332f08b71b58f5682
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2020-09-30 16:23:59 -0300

    Delete 7kyu-SQLwithPokemonDamageMultipliers.sql

--- sql/7kyu-SQLwithPokemonDamageMultipliers.sql
+++
@@ -1,7 +0,0 @@
-SELECT pokemon_name, 
-       str * multiplier AS modifiedStrength,
-       element 
-FROM pokemon 
-INNER JOIN multipliers ON pokemon.element_id = multipliers.id
-WHERE str * multiplier >= 40
-ORDER BY modifiedStrength DESC
