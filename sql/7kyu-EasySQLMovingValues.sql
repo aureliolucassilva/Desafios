988332bac966d83f08564b2301912fd2beb5f6f7
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2020-09-30 16:22:18 -0300

    Delete 7kyu-EasySQLMovingValues.sql

--- sql/7kyu-EasySQLMovingValues.sql
+++
@@ -1,6 +0,0 @@
-SELECT LENGTH(name) AS id,
-       LENGTH(CAST(legs AS TEXT)) AS name,
-       LENGTH(CAST(arms AS TEXT)) AS legs,
-       LENGTH(characteristics) AS arms,
-       LENGTH(CAST(id AS TEXT)) AS characteristics
-FROM monsters
