154afc572923b4ced7cc8d0c461886bd1c3d79cf
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2020-09-30 17:32:34 -0300

    Create 7kyu - E.js

---
+++ javascript/7kyu - E.js
@@ -0,0 +1,9 @@
+//Link -> https://www.codewars.com/kata/55955a48a4e9c1a77500005a
+
+function greet(name) {
+    if(name !== "" && name){
+        return "hello " + name + "!";
+    } else {
+        return null;
+    }
+}
