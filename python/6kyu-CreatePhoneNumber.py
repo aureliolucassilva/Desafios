844ec9dc4e5a83a28bc24dc9ac420840422d905f
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2020-09-30 16:16:13 -0300

    Delete 6kyu-CreatePhoneNumber.py

--- python/6kyu-CreatePhoneNumber.py
+++
@@ -1,8 +0,0 @@
-def create_phone_number(n):
-    phone_number = ''
-
-    phone_number += '(' + ''.join(list(map(str, n[0:3]))) + ') '
-    phone_number += ''.join(list(map(str, n[3:6]))) + '-'
-    phone_number += ''.join(list(map(str, n[6:10])))
-
-    return phone_number
