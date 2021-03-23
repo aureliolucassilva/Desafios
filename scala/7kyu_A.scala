a0d0fba67a412a14a174818d99d560e1528d95aa
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2021-03-23 08:42:26 -0300

    Create 7kyu_A.scala

---
+++ scala/7kyu_A.scala
@@ -0,0 +1,17 @@
+import scala.math._
+
+object Hello extends App{
+
+  def nbYear(p0: Int, percent: Double, aug: Int, p: Int): Int = {
+    var years: Int = 0
+    var newPopulation: Double = p0
+
+    while (newPopulation < p) {
+      newPopulation = floor(newPopulation + newPopulation * (percent / 100) + aug)
+      years = years + 1
+    }
+    return years
+  }
+
+  println(nbYear(1000,2.0,50,1214))
+}
