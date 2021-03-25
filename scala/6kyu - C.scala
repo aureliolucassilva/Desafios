97facc61f52e915717f5cece2ef426fe07d32858
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2021-03-25 09:25:42 -0300

    Create 6kyu - C.scala

---
+++ scala/6kyu - C.scala
@@ -0,0 +1,35 @@
+object Hello extends App{
+
+  def stockSummary(lstOfArt: Array[String], lstOfCat: Array[String]): String = {
+    var stock: Map[String, Int] = Map[String, Int]()
+    var stockReport = ""
+
+    if (lstOfArt.length == 0) return ""
+
+    for (book <- lstOfArt) {
+      val category: String = book.take(1)
+      val quantity: Int = book.split(" ")(1).toInt
+
+      if (stock.contains(category)) {
+        val newValue = stock(category) + quantity
+        stock = stock + (category -> newValue)
+      }
+      else {
+        stock += (category -> quantity)
+      }
+    }
+
+    for(category <- lstOfCat){
+      if (!stock.contains(category)){
+        stockReport = stockReport + " - " + "(" + category + " : 0" + ")"
+      }
+      else {
+        stockReport = stockReport + " - " + "(" + category + " : " + stock(category) + ")"
+      }
+    }
+
+    stockReport = stockReport.drop(2)
+
+    return stockReport
+  }
+}
