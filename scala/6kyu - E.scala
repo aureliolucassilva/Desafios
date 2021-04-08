ece8d53b4fee553c182e5d1ff08b315952d09c43
aureliolucassilva <61013276+aureliolucassilva@users.noreply.github.com>
2021-04-08 08:22:54 -0300

    Create 6kyu - E.scala

---
+++ scala/6kyu - E.scala
@@ -0,0 +1,26 @@
+// Link -> https://www.codewars.com/kata/57eb8fcdf670e99d9b000272
+
+object Hello extends App{
+
+  def high(s: String): String = {
+    val alphabet = "abcdefghijklmnopqrstuvwxyz"
+    val words = s.split(" ")
+    var highestSum = 0
+    var highest = ""
+
+    for (a <- words){
+      var sum = 0
+      for(b <- a){
+        sum = sum + alphabet.indexOf(b) + 1
+      }
+      if (sum > highestSum) {
+        highest = a
+        highestSum = sum
+      }
+    }
+
+    highest
+  }
+
+  high("find or finda")
+}
